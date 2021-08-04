6d2a8e28f7ecbbb1be8635a0477194ccb7f179be
unknown <90manbo@gmail.com>
2021-08-04 12:06:00 +0900

    Update

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// const toggleBtn = document.querySelector('.navbar_toggleBtn');
-// const list = document.querySelector('.navbar_list');
-// const sns = document.querySelector('.navbar_sns');
-
-// toggleBtn.addEventListener('click', ()=>{
-//     list.classList.toggle('active');
-//     sns.classList.toggle('active');
-// });
-
-
 // Modal을 가져옵니다.
 var modals = document.getElementsByClassName("modal");
 // Modal을 띄우는 클래스 이름을 가져옵니다.
@@ -16,7 +6,7 @@ var modalImg = document.getElementsByClassName("modal-content");
 // Modal을 닫는 close 클래스를 가져옵니다.
 var spanes = document.getElementsByClassName("close");
 var funcs = [];
- 
+
 // Modal을 띄우고 닫는 클릭 이벤트를 정의한 함수
 function Modal(num) {
   return function() {
@@ -29,24 +19,23 @@ function Modal(num) {
     };
 
     
- 
     // <span> 태그(X 버튼)를 클릭하면 Modal이 닫습니다.
     spanes[num].onclick = function() {
         modals[num].style.display = "none";
     };
   };
 }
- 
+
 // 원하는 Modal 수만큼 Modal 함수를 호출해서 funcs 함수에 정의합니다.
 for(var i = 0; i < btns.length; i++) {
   funcs[i] = Modal(i);
 }
- 
+
 // 원하는 Modal 수만큼 funcs 함수를 호출합니다.
 for(var j = 0; j < btns.length; j++) {
   funcs[j]();
 }
- 
+
 // Modal 영역 밖을 클릭하면 Modal을 닫습니다.
 window.onclick = function(event) {
   if (event.target.className == "modal") {
